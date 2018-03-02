393f768a1f6b206cfef622e18a46fc579fa40bdb
Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
2018-03-02 16:20:18 +0100

    incudine-setup added

---
+++ incudine-setup.lisp
@@ -0,0 +1,38 @@
+;;;; levelmeter-gui.lisp
+;;;;
+;;;; Copyright (c) 2018 Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
+
+(in-package #:scratch)
+(export 'setup-io :scratch)
+(export 'node-free-unprotected :scratch)
+ 
+(defmacro foreach-input-channel (&body body)
+  (with-gensyms (i)
+    `(dochannels (,i *number-of-input-bus-channels*)
+       (let ((current-channel ,i))
+         (declare (type channel-number current-channel)
+                  (ignorable current-channel))
+         ,@body))))
+
+(dsp! cp-input-buses ((first-in-bus channel-number))
+  (:defaults 0)
+  (foreach-input-channel
+    (setf (bus (+ current-channel first-in-bus)) (audio-in current-channel))))
+
+(dsp! cp-output-buses ((first-out-bus channel-number))
+  (:defaults 8)
+  (foreach-channel
+    (setf (bus (+ current-channel first-out-bus)) (audio-out current-channel))))r
+
+
+(defun setup-io ()
+  (make-group 100)
+  (make-group 200 :after 100)
+  (make-group 300 :after 200)
+  (make-group 400 :after 300)
+  (cp-input-buses :head 100)
+  (cp-output-buses :head 300))
+
+(defun node-free-unprotected ()
+ (dogroup (n (node 200))
+   (free n)))
