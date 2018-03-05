967695a984d86353fd8717d25fb26369e730c88c
Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
2018-03-05 20:56:56 +0100

    added input selector of stethoscope and redraw on scroll

--- stethoscope-gui.lisp
+++ stethoscope-gui.lisp
@@ -25,28 +25,43 @@
   ((num-chans-box :initform (make-instance 'numbox) :accessor num-chans-box)
    (bus-num-box :initform (make-instance 'numbox) :accessor bus-num-box)
    (mode-button :initform (make-instance 'pushbutton) :accessor mode-button)
+   (io-button :initform (make-instance 'pushbutton) :accessor io-button)
    (tracks-action :accessor tracks-action)
    (overlay-action :accessor overlay-action)
-   (xy-action :accessor xy-action)))
+   (xy-action :accessor xy-action)
+   (audio-in-action :accessor audio-in-action)
+   (audio-out-action :accessor audio-out-action)
+   (bus-action :accessor bus-action)))
 
 (define-subwidget (stethoscope-ctl layout) (q+:make-qhboxlayout stethoscope-ctl)
-  (let ((menu (q+:make-qmenu)))
-    (setf tracks-action (q+:make-qaction "Tracks" menu))
-    (setf overlay-action (q+:make-qaction "Overlay" menu))
-    (setf xy-action (q+:make-qaction "X/Y" menu))
-    (q+:add-action menu tracks-action)
-    (q+:add-action menu overlay-action)
-    (q+:add-action menu xy-action)
-    (q+:set-menu mode-button menu)
+  (let ((io-menu (q+:make-qmenu)))
+    (setf audio-in-action (q+:make-qaction "Audio in" io-menu))
+    (setf audio-out-action (q+:make-qaction "Audio out" io-menu))
+    (setf bus-action (q+:make-qaction "Bus" io-menu))
+    (q+:add-action io-menu audio-in-action)
+    (q+:add-action io-menu audio-out-action)
+    (q+:add-action io-menu bus-action)
+    (q+:set-menu io-button io-menu)
+    (q+:set-text io-button "Audio in"))
+  (let ((view-menu (q+:make-qmenu)))
+    (setf tracks-action (q+:make-qaction "Tracks" view-menu))
+    (setf overlay-action (q+:make-qaction "Overlay" view-menu))
+    (setf xy-action (q+:make-qaction "X/Y" view-menu))
+    (q+:add-action view-menu tracks-action)
+    (q+:add-action view-menu overlay-action)
+    (q+:add-action view-menu xy-action)
+    (q+:set-menu mode-button view-menu)
     (q+:set-text mode-button "Tracks"))
   (q+:add-widget layout bus-num-box)
   (q+:add-widget layout num-chans-box)
+  (q+:add-widget layout io-button)
   (q+:add-stretch layout)
   (q+:add-widget layout mode-button)
   (q+:add-widget layout (make-instance 'scroll-corner-box))
   (q+:set-margin layout 0)
   (q+:set-spacing layout 2)
   (q+:set-fixed-width mode-button 85)
+  (q+:set-fixed-width io-button 95)
   (q+:set-fixed-height stethoscope-ctl 25))
 
 (define-widget stethoscope-view (QWidget) ;;; the plot area
@@ -170,6 +185,7 @@
    (num-chans :initform 2 :initarg :num-chans :accessor num-chans)
    (bus-num :initform 0 :initarg :bus-num :accessor bus-num)
    (draw-mode :initform :tracks :accessor draw-mode)
+   (io-mode :initform :audio-in :accessor io-mode)
    (zoom-x :initarg zoom-x :accessor zoom-x)
    (zoom-y :initarg zoom-y :accessor zoom-y)
    (chans-minval :initform 1 :accessor chans-minval)
@@ -207,6 +223,40 @@
   (and (find-node dsp-node-id) (incudine:free dsp-node-id))
   (call-next-qmethod))
 
+(define-slot (stethoscope audio-in-action) ()
+  (declare (connected
+            (audio-in-action (steth-ctl stethoscope))
+            (triggered)))
+  (setf (redraw? stethoscope) nil)
+  (setf (io-mode stethoscope) :audio-in)
+  (q+:set-text (io-button (steth-ctl stethoscope)) "Audio in")
+;;;  (setf (num-chans stethoscope) (textedit-parse-integer text (chans-minval stethoscope)))
+  (restart-stethoscope-dsp stethoscope)
+  (setf (redraw? stethoscope) t)
+  (q+:repaint (steth-view-pane stethoscope)))
+
+(define-slot (stethoscope audio-out-action) ()
+  (declare (connected
+            (audio-out-action (steth-ctl stethoscope))
+            (triggered)))
+  (setf (redraw? stethoscope) nil)
+  (setf (io-mode stethoscope) :audio-out)
+  (q+:set-text (io-button (steth-ctl stethoscope)) "Audio out")
+  (restart-stethoscope-dsp stethoscope)
+  (setf (redraw? stethoscope) t)
+  (q+:repaint (steth-view-pane stethoscope)))
+
+(define-slot (stethoscope bus-action) ()
+  (declare (connected
+            (bus-action (steth-ctl stethoscope))
+            (triggered)))
+  (setf (redraw? stethoscope) nil)
+  (setf (io-mode stethoscope) :bus)
+  (q+:set-text (io-button (steth-ctl stethoscope)) "Bus")
+  (restart-stethoscope-dsp stethoscope)
+  (setf (redraw? stethoscope) t)
+  (q+:repaint (steth-view-pane stethoscope)))
+
 (define-slot (stethoscope tracks-action) ()
   (declare (connected
             (tracks-action (steth-ctl stethoscope))
@@ -241,21 +291,38 @@
           '(bufs-a bufs-b))
     (setf bufs-a (make-array num-chans)
           bufs-b (make-array num-chans)
-          curr-bufs bufs-a
-          )
+          curr-bufs bufs-a)
     (incudine::dochannels (idx num-chans)
       (setf (svref bufs-a idx) (incudine:make-buffer (1+ bufmaxsize) :real-time-p t))
       (setf (svref bufs-b idx) (incudine:make-buffer (1+ bufmaxsize) :real-time-p t))))
   (let ((node (dsp-node-id stethoscope)))
-           (and (find-node node) (incudine:free node))
-           (sleep 0.2)
-           (scratch::scope-dsp (num-chans stethoscope) (bus-num stethoscope) stethoscope
-                               (bufsize stethoscope)
-                               (bufmaxsize stethoscope)
-                               :action (lambda (n)
-                                         (setf (dsp-node-id stethoscope) (incudine:node-id n)))
-                               :tail group
-                               :id (dsp-node-id stethoscope))))
+    (and (find-node node) (incudine:free node))
+    (sleep 0.2)
+    (case (io-mode stethoscope)
+      (:audio-out
+       (scratch::audio-out-scope-dsp (num-chans stethoscope) (bus-num stethoscope) stethoscope
+                           (bufsize stethoscope)
+                           (bufmaxsize stethoscope)
+                           :action (lambda (n)
+                                     (setf (dsp-node-id stethoscope) (incudine:node-id n)))
+                           :tail group
+                           :id (dsp-node-id stethoscope)))
+      (:bus
+       (scratch::bus-scope-dsp (num-chans stethoscope) (bus-num stethoscope) stethoscope
+                           (bufsize stethoscope)
+                           (bufmaxsize stethoscope)
+                           :action (lambda (n)
+                                     (setf (dsp-node-id stethoscope) (incudine:node-id n)))
+                           :tail group
+                           :id (dsp-node-id stethoscope)))
+      (t
+       (scratch::audio-in-scope-dsp (num-chans stethoscope) (bus-num stethoscope) stethoscope
+                           (bufsize stethoscope)
+                           (bufmaxsize stethoscope)
+                           :action (lambda (n)
+                                     (setf (dsp-node-id stethoscope) (incudine:node-id n)))
+                           :tail group
+                           :id (dsp-node-id stethoscope))))))
 
 (defmacro toggle (slot)
   `(setf ,slot (not ,slot)))
@@ -306,18 +373,16 @@
                 (q+:maximum scrollbar)))
          (new-size (round (+ 128 (* prop (- (bufmaxsize stethoscope) 128))))))
     (setf (bufsize stethoscope) new-size)
-    (incudine:set-control (dsp-node-id stethoscope) :bufsize new-size)))
+    (incudine:set-control (dsp-node-id stethoscope) :bufsize new-size)
+    (q+:repaint (steth-view-pane stethoscope))))
 
-#|
+
 (define-slot (stethoscope y-scroll-changed) ((value int))
   (declare (connected
              (scroll-y (steth-view-pane stethoscope))
             (value-changed int)))
-  (let* ((scrollbar (scroll-y (steth-view-pane stethoscope)))
-         (new-size (round (+ 128 (* prop (- (bufmaxsize stethoscope) 128))))))
-    (setf (bufsize stethoscope) new-size)
-    (incudine:set-control (dsp-node-id stethoscope) :bufsize new-size)))
-|#
+  (q+:repaint (steth-view-pane stethoscope)))
+
 
 (define-signal (stethoscope repaint-view) ())
 
@@ -331,6 +396,8 @@
   (gui-funcall (create-tl-widget 'stethoscope id :group group :bus-num bus :num-chans num-chans)))
 
 ;;; (scope :num-chans 2)
+
+;;; (close-all-guis)
 
 #|
 
--- stethoscope-incudine.lisp
+++ stethoscope-incudine.lisp
@@ -6,35 +6,7 @@
 
 (deftype uint () '(integer 0 #.most-positive-fixnum))
 
-(define-vug env-levelmeter (in (freq fixnum) (meter incudine-gui::levelmeter)
-                            (periods channel-number))
-  (:defaults +sample-zero+ 10 nil 2)
-  (with ((size (round-sample (/ (* periods *sample-rate*) freq)))
-         (hanning (make-local-buffer (1+ size) :fill-function (hanning-rms)))
-         (sums (make-frame periods :zero-p t))
-         (bufidx (make-array periods :element-type 'channel-number))
-         (max +sample-zero+)
-         (value 0))
-    (declare  (fixnum size value) (sample max))
-    (initialize
-      (dotimes (i periods)
-        (setf (aref bufidx i)
-              (reduce-warnings (floor (* i (round (/ size periods))))))))
-    (dotimes (i periods)
-      (incf (smp-ref sums i) (* in in (buffer-value hanning (aref bufidx i))))
-      (when (>= (incf (aref bufidx i)) size)
-        (progn
-          (setf value
-                (round-sample
-                  (+ 100 (lin->db
-                          (sqrt (the non-negative-sample (smp-ref sums i)))))))
-          (nrt-funcall
-            (lambda ()
-              (cuda-gui:change-level meter value)))
-          (setf (smp-ref sums i) +sample-zero+)
-          (setf (aref bufidx i) 0))))))
-
-(define-vug scope-vug ((numchans channel-number)
+(define-vug audio-out-scope-vug ((numchans channel-number)
                        (in channel-number)
                        (gui incudine-gui::stethoscope)
                        (size uint)
@@ -42,23 +14,16 @@
   (with ((sample-idx 0)
          (sample-max-idx (min size maxsize))
          (curr-p t)
+         (bufs-a (cuda-gui::bufs-a gui))
+         (bufs-b (cuda-gui::bufs-b gui))
          (curr-bufs (make-array numchans)))
     (declare (uint sample-idx sample-max-idx) (boolean curr-p))
     (initialize
-     (reduce-warnings
-       (dochannels (idx numchans)
-         (setf (svref bufs-a idx) (make-buffer (1+ maxsize) :real-time-p t))
-         (setf (svref bufs-b idx) (make-buffer (1+ maxsize) :real-time-p t))))
-     (rplacd (free-hook (dsp-node))
-             (lambda (n) n (mapc 'free (list bufs-a bufs-b))))
-     (setf (incudine-gui::bufs gui) bufs-b)
+     (setf (incudine-gui::curr-bufs gui) bufs-b)
      (setf curr-bufs bufs-a))
     (dochannels (idx numchans)
       (setf (buffer-value (svref curr-bufs idx) sample-idx)
-            (audio-in (+ in idx)))
-;;;   (nrt-funcall
-;;;       (lambda () (format t "current-channel: ~a" idx))
-      )
+            (audio-out (+ in idx))))
     (incf sample-idx)
     (when (>= sample-idx sample-max-idx)
       (reduce-warnings
@@ -67,13 +32,13 @@
       (setf sample-idx 0)
       (setf curr-p (not curr-p))
       (cond (curr-p ;;; swap display and audio buffer
-             (setf (incudine-gui::bufs gui) bufs-b)
+             (setf (incudine-gui::curr-bufs gui) bufs-b)
              (setf curr-bufs bufs-a))
             (t
-             (setf (incudine-gui::bufs gui) bufs-a)
+             (setf (incudine-gui::curr-bufs gui) bufs-a)
              (setf curr-bufs bufs-b))))))
 
-(define-vug scope-vug ((numchans channel-number)
+(define-vug audio-in-scope-vug ((numchans channel-number)
                        (in channel-number)
                        (gui incudine-gui::stethoscope)
                        (size uint)
@@ -105,12 +70,56 @@
              (setf (incudine-gui::curr-bufs gui) bufs-a)
              (setf curr-bufs bufs-b))))))
 
+(define-vug bus-scope-vug ((numchans channel-number)
+                       (in channel-number)
+                       (gui incudine-gui::stethoscope)
+                       (size uint)
+                       (maxsize uint))
+  (with ((sample-idx 0)
+         (sample-max-idx (min size maxsize))
+         (curr-p t)
+         (bufs-a (cuda-gui::bufs-a gui))
+         (bufs-b (cuda-gui::bufs-b gui))
+         (curr-bufs (make-array numchans)))
+    (declare (uint sample-idx sample-max-idx) (boolean curr-p))
+    (initialize
+     (setf (incudine-gui::curr-bufs gui) bufs-b)
+     (setf curr-bufs bufs-a))
+    (dochannels (idx numchans)
+      (setf (buffer-value (svref curr-bufs idx) sample-idx)
+            (bus (+ in idx))))
+    (incf sample-idx)
+    (when (>= sample-idx sample-max-idx)
+      (reduce-warnings
+        (nrt-funcall
+          (lambda () (cuda-gui::signal! gui (cuda-gui::repaint-view)))))
+      (setf sample-idx 0)
+      (setf curr-p (not curr-p))
+      (cond (curr-p ;;; swap display and audio buffer
+             (setf (incudine-gui::curr-bufs gui) bufs-b)
+             (setf curr-bufs bufs-a))
+            (t
+             (setf (incudine-gui::curr-bufs gui) bufs-a)
+             (setf curr-bufs bufs-b))))))
+
 ;;;          (nrt-funcall (lambda () (cuda-gui::gui-funcall (q+:repaint gui))))
 
 ;;; (dotimes (idx 3) (print idx) (print idx))
 
-(dsp! scope-dsp ((numchans channel-number) (bus-num channel-number) (gui incudine-gui::stethoscope)
+(dsp! audio-in-scope-dsp ((numchans channel-number) (bus-num channel-number) (gui incudine-gui::stethoscope)
                  (bufsize uint) (bufmaxsize uint) (process? boolean))
   (:defaults 2 8 nil 1024 8192 t)
   (if process?
-      (foreach-frame (scope-vug numchans bus-num gui bufsize bufmaxsize))))
+      (foreach-frame (bus-scope-vug numchans bus-num gui bufsize bufmaxsize))))
+
+(dsp! audio-out-scope-dsp ((numchans channel-number) (bus-num channel-number) (gui incudine-gui::stethoscope)
+                 (bufsize uint) (bufmaxsize uint) (process? boolean))
+  (:defaults 2 8 nil 1024 8192 t)
+  (if process?
+      (foreach-frame (audio-out-scope-vug numchans bus-num gui bufsize bufmaxsize))))
+
+(dsp! bus-scope-dsp ((numchans channel-number) (bus-num channel-number) (gui incudine-gui::stethoscope)
+                 (bufsize uint) (bufmaxsize uint) (process? boolean))
+  (:defaults 2 8 nil 1024 8192 t)
+  (if process?
+      (foreach-frame (bus-scope-vug numchans bus-num gui bufsize bufmaxsize))))
